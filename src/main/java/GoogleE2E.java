b7e44dd324b56f6cc828b056bc510a3733870d63
nateluisa <77622688+nateluisa@users.noreply.github.com>
2022-04-17 18:46:44 -0300

    first commit

---
+++ src/main/java/GoogleE2E.java
@@ -0,0 +1,5 @@
+public class GoogleE2E {
+    public static void main (String args[]){
+
+    }
+}
---
+++ src/test/java/TestingVue.java
@@ -0,0 +1,40 @@
+
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+
+class TestingVue {
+    static ChromeDriver driver;
+    @BeforeAll
+    public static void  before() {
+        System.setProperty("webdriver.chrome.driver","C:\\Chrome Driver\\chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+    @Test
+    public void testingPicInsert() {
+        driver = new ChromeDriver();
+        driver.get("http://localhost:8080/");
+        findCadastro()
+        driver.findElement(By.linkText("Cadastro")).click();
+        System.out.println("Informe uma nova imagem ao" + driver.getTitle());
+        driver.findElement(By.id("titulo")).sendKeys("Foto para teste de automação");
+        driver.findElement(By.id("url")).sendKeys("https://upload.wikimedia.org/wikipedia/commons/d/d5/Selenium_Logo.png");
+        driver.findElement(By.id("descricao")).sendKeys("Teste de automação para salvamento de imagem");
+        driver.findElement(By.id("gravar")).click();
+        System.out.println("Foto salva no" + driver.getTitle());
+        driver.findElement(By.linkText("Home")).click();
+
+
+
+
+
+
+
+    }
+}
